e8c5136bcd20333494d6ffe63d000a5eea6f2e35
Rocktsx <dougtsx@outlook.com>
2025-05-27 17:14:30 +0800

    添加公司模型dto

---
+++ src/Accounting.Application.Contracts/BasicData/Company/CompanyCreateOrEditDto.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Accounting.BasicData.Company
+{
+    public class CompanyCreateOrEditDto
+    {
+        [Required]
+        [MaxLength(CompanyConsts.MaxNameLength)]
+        public string Name { get; set; }
+        [MaxLength(CompanyConsts.MaxNameLength)]
+        public string OtherName { get; set; }
+        [MaxLength(CompanyConsts.CommonMaxLength)]
+        public string NickName { get; set; }
+        [MaxLength(CurrencyConsts.MaxCurrencyLength)]
+        public string Currency { get; set; }
+        public decimal CreditLimit { get; set; }
+        [MaxLength(CompanyConsts.CommonMaxLength)]
+        public string PaymentTerm { get; set; }
+        [MaxLength(CompanyConsts.CommonMaxLength)]
+        public string TradeTerm { get; set; }
+        public bool IsClient { get; set; }
+        public bool IsVendor { get; set; }
+
+        public IEnumerable<CompanyAddressCreateOrEditDto> Addresses { get; set; } = new List<CompanyAddressCreateOrEditDto>();
+        public IEnumerable<CompanyContactCreateOrEditDto> Contacts { get; set; } = new List<CompanyContactCreateOrEditDto>();
+    }
+}
---
+++ src/Accounting.Application.Contracts/BasicData/Company/CompanyDto.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Accounting.BasicData;
+
+namespace Accounting.BasicData.Company
+{
+    public class CompanyDto
+    {
+        public Guid Id { get; set; } 
+        public string Name { get; set; } 
+        public string OtherName { get; set; } 
+        public string NickName { get; set; }
+        public string Currency { get; set; }
+        public decimal CreditLimit { get; set; } 
+        public string PaymentTerm { get; set; } 
+        public string TradeTerm { get; set; }
+        public bool IsClient { get; set; }
+        public bool IsVendor { get; set; }
+
+        public IEnumerable<CompanyAddressDto> Addresses { get; set; } = new List<CompanyAddressDto>();
+        public IEnumerable<CompanyContactDto> Contacts { get; set; } = new List<CompanyContactDto>();
+    }
+}
