1d7d830ab8d8f86ce373305b1d180efbf3f60d4d
Rocktsx <dougtsx@outlook.com>
2025-05-27 17:12:38 +0800

    添加公司地址模型dto

---
+++ src/Accounting.Application.Contracts/BasicData/Company/CompanyAddressDto.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Accounting.BasicData.Company
+{
+    public class CompanyAddressDto
+    {
+        public Guid CompanyId { get;set; }
+        public Guid AddressId { get;set; }
+        public bool IsBilling { get;set; }
+        public bool IsShipping { get;set; }
+        public string Name { get;set; }
+        public string Address { get;set; }
+        public string ContactPerson { get;set; }
+        public string Telephone { get;set; }
+        public string Fax { get;set; }
+        public string Email { get;set; }
+        public string Remark { get;set; }
+        public string Country { get;set; }
+        public string Region { get;set; }
+        public string District { get;set; }
+    }
+}
