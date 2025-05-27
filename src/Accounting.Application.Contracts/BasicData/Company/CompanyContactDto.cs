ceff712e9be4663270cdb19fa3a741ccb4b67d07
Rocktsx <dougtsx@outlook.com>
2025-05-27 17:14:20 +0800

    添加公司联系人模型dto

---
+++ src/Accounting.Application.Contracts/BasicData/Company/CompanyContactDto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Accounting.BasicData.Company
+{
+    public class CompanyContactDto
+    {
+        public Guid CompanyId { get; set; }
+        public Guid ContactId { get; set; }
+        public string ContactName { get; set; }
+        public string Department { get; set; }
+        public string Position { get; set; }
+        public string DirectLine { get; set; }
+        public string Telephone { get; set; }
+        public string Fax { get; set; }
+        public string Email { get; set; }
+        public string Remark { get; set; }
+    }
+}
