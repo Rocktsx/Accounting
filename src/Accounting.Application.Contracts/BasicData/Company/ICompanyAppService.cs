ffc111cb4cab9ffd7d442e02bd55f45b0d92b9f0
Rocktsx <dougtsx@outlook.com>
2025-05-28 16:26:54 +0800

    添加公司应用服务接口

---
+++ src/Accounting.Application.Contracts/BasicData/Company/ICompanyAppService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Application.Services;
+
+namespace Accounting.BasicData.Company
+{
+    public interface ICompanyAppService :  IApplicationService
+    {
+        Task<CompanyDto> GetAsync(Guid companyId);
+        Task<List<CompanyDto>> GetListAsync(Guid companyId);
+        Task<CompanyDto> CreateAsync(CompanyCreateOrEditDto input);
+        Task<CompanyDto> UpdateAsync(Guid companyId,  CompanyCreateOrEditDto input);
+        Task DeleteAsync(Guid companyId);
+    } 
+}
