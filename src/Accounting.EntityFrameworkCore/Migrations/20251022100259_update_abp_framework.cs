a3a7d5d85a170eeb00418a6aa93be358ae023c94
Rock <Dougtsx@outlook.com>
2025-10-22 18:05:44 +0800

    添加ABP Framework到9.3.5版本数据库的更新

---
+++ src/Accounting.EntityFrameworkCore/Migrations/20251022100259_update_abp_framework.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Accounting.Migrations
+{
+    /// <inheritdoc />
+    public partial class update_abp_framework : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ApplicationName",
+                table: "AbpBackgroundJobs",
+                type: "nvarchar(96)",
+                maxLength: 96,
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "AbpAuditLogExcelFiles",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    TenantId = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    FileName = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: true),
+                    CreationTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CreatorId = table.Column<Guid>(type: "uniqueidentifier", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AbpAuditLogExcelFiles", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AbpAuditLogExcelFiles");
+
+            migrationBuilder.DropColumn(
+                name: "ApplicationName",
+                table: "AbpBackgroundJobs");
+        }
+    }
+}
