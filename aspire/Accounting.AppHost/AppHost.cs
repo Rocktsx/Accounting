f661add2cb000edbe88f565871cf970b0b8a45fa
Rock <Dougtsx@outlook.com>
2025-11-06 16:28:15 +0800

    添加DbMigrator项目到aspire

--- aspire/Accounting.AppHost/AppHost.cs
+++ aspire/Accounting.AppHost/AppHost.cs
@@ -1,4 +1,14 @@
+using Microsoft.Extensions.Hosting;
+
 var builder = DistributedApplication.CreateBuilder(args);
+
+// DbMigrator  
+if (builder.Environment.IsDevelopment())
+{
+    builder
+        .AddProject<Projects.Accounting_DbMigrator>("DbMigrator")
+        .WithReplicas(1);
+}
 
 builder.AddProject<Projects.Accounting_Web>("accounting-web");
 
