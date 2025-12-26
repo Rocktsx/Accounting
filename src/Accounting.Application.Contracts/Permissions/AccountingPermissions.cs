8c97cab2ae7e4735af1dbb73e6d48f5c4748b105
Rock <Dougtsx@outlook.com>
2025-12-26 11:13:30 +0800

    修复权限定义有空格问题

--- src/Accounting.Application.Contracts/Permissions/AccountingPermissions.cs
+++ src/Accounting.Application.Contracts/Permissions/AccountingPermissions.cs
@@ -297,13 +297,13 @@ public static class AccountingPermissions
         public const string Update = Default + Edit;
         public const string UpdateStatus = Default + UpdateStatusDot;
     }
-
+    
     /// <summary>
     /// 付款传票帐龄分析报表
     /// </summary>
     public class PayableAgingReports
     {
-        public const string Name = " PayableAgingReport";
+        public const string Name = "PayableAgingReport";
         public const string Default = PayableGroup + Dot + Name;
         private const string DispalyName = PermissionPrefix + Name + Dot;
         internal const string AgingSummarySingleCurrencyName = DispalyName + AgingSummarySingleCurrencyText;
