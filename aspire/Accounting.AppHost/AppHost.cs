13230049d735c971acf7a897ebb046f02d57ee45
Rock <Dougtsx@outlook.com>
2025-12-27 14:14:04 +0800

    调整DbMigrator限制

--- aspire/Accounting.AppHost/AppHost.cs
+++ aspire/Accounting.AppHost/AppHost.cs
@@ -3,12 +3,8 @@ using Microsoft.Extensions.Hosting;
 var builder = DistributedApplication.CreateBuilder(args);
 
 // DbMigrator  
-if (builder.Environment.IsDevelopment())
-{
-    builder
-        .AddProject<Projects.Accounting_DbMigrator>("DbMigrator")
+builder.AddProject<Projects.Accounting_DbMigrator>("DbMigrator")
         .WithReplicas(1);
-}
 
 builder.AddProject<Projects.Accounting_Web>("accounting-web");
 
