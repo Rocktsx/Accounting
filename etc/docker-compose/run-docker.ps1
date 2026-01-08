c8316467f1ec7f4bfcad74bfef0543b9a87bd5e9
Rock <Dougtsx@outlook.com>
2026-01-08 18:04:29 +0800

    修改dockerfile配置，添加docker-compose配置及脚本

---
+++ etc/docker-compose/run-docker.ps1
@@ -0,0 +1,16 @@
+$currentFolder = $PSScriptRoot
+
+$slnFolder = Join-Path $currentFolder "../"
+$certsFolder = Join-Path $currentFolder "certs"
+
+If(!(Test-Path -Path $certsFolder))
+{
+    New-Item -ItemType Directory -Force -Path $certsFolder
+    if(!(Test-Path -Path (Join-Path $certsFolder "localhost.pfx") -PathType Leaf)){
+        Set-Location $certsFolder
+        dotnet dev-certs https -v -ep localhost.pfx -p BB7BBDD7-5D33-48D6-9B6F-C49E3EF468ED -t        
+    }
+}
+
+Set-Location $currentFolder
+docker-compose up
